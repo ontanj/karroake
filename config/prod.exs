7162e6ca027b0babc7a559ef0cd3781cefc6b63d
ontanj <anton.jeppsson@outlook.com>
2019-08-13 19:57:20 +0200

    heroku

--- config/prod.exs
+++ config/prod.exs
@@ -1,4 +1,4 @@
-use Mix.Config
+import Config
 
 # For production, don't forget to configure the url host
 # to something meaningful, Phoenix uses this information
@@ -10,8 +10,13 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :karroake, KarroakeWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "karroake.herokuapp.com", port: 443],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  load_from_system_env: true,
+  http: [:inet6, port: System.get_env("PORT")],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+  
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -50,6 +55,12 @@ config :logger, level: :info
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
 
+# Configure your database
+config :karroake, Karroake.Repo,
+  url: System.get_env("DATABASE_URL"),
+  ssl: true,
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
-import_config "prod.secret.exs"
+#import_config "prod.secret.exs"
